fe9b7b00f910c2fda61d754a6ab6db5371540801
Hiba REZQUELLAH <hiba.rezquellah.elv@eilco-ulco.fr>
2022-01-29 00:33:37 +0100

    Initial Commit

---
+++ Projet_Reing/Projet_Reing/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Projet_Reing.Startup))]
+namespace Projet_Reing
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
